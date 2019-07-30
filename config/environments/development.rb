e1452ea295b249be87a354685b4eecfc65232860
takamori namubu <roadfox303@gmail.com>
2019-07-30 15:45:20 +0900

    developmentログレベルの変更warn→info

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.log_level = :warn
+  config.log_level = :info
   config.log_formatter = Logger::Formatter.new
 end
